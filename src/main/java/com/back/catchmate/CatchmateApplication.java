246993e7252b7cf1e939b12e2ae32388701441f0
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-12-27 00:26:57 +0900

    chore : 프로젝트 초기 세팅

---
+++ src/main/java/com/back/catchmate/CatchmateApplication.java
@@ -0,0 +1,13 @@
+package com.back.catchmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchmateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/catchmate/CatchmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.catchmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
