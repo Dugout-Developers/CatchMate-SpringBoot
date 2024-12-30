84b9c94f7c8e097bc7aafb63ca1054beebf234ca
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2024-12-30 19:59:50 +0900

    feat : 구단 목록 조회 기능 구현

---
+++ src/main/java/com/back/catchmate/domain/club/service/ClubService.java
@@ -0,0 +1,7 @@
+package com.back.catchmate.domain.club.service;
+
+import com.back.catchmate.domain.club.dto.ClubResponse;
+
+public interface ClubService {
+    ClubResponse.ClubInfoList getClubInfoList();
+}
